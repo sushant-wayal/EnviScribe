1469befbbf2f25d6492b3c6153ec870f1cf49a0a
sushant-wayal <sushantssr12345@gmail.com>
2024-04-10 00:03:22 +0530

    implemented logs download in excel

--- FrontEnd/src/components/Pages/logs.tsx
+++ FrontEnd/src/components/Pages/logs.tsx
@@ -7,6 +7,7 @@ import axios from "axios";
 import { useParams } from "react-router-dom";
 import ReadingsTable from "../Components/ReadingsTable";
 import Logs from "../../data/logs.json";
+import * as XLSX from 'xlsx';
 
 interface logsPageProps {}
 
@@ -14,11 +15,34 @@ type Log = {
   value: string;
   timestamp: string;
   status: string;
+  [key: string]: string | undefined;
 }
 
 const LogsPage: React.FC<logsPageProps> = () => {
-  const [logs, setLogs] = useState<Log[]>(Logs.logs);
+  const [logs, setLogs] = useState<Log[]>(Logs.logs.map((log) => ({ ...log, value: log.value.toString()})));
   const { sensorId } = useParams();
+
+  const convertJSONtoExcel = (jsonData : Log[]) => {
+    const worksheet = XLSX.utils.json_to_sheet(jsonData);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Sheet1');
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+    return excelBuffer;
+  };
+  const downloadExcel = (excelBuffer : any, fileName : String) => {
+    const data = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    const url = window.URL.createObjectURL(data);
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', `${fileName}.xlsx`);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+  const DownloadLogs = () => {
+    const excelBuffer = convertJSONtoExcel(logs);
+    downloadExcel(excelBuffer, 'logs');
+  };
   useEffect(() => {
     const getLogs = async () => {
       const { data } = await axios.get(`http://localhost:3000/api/v1/logs/${sensorId}`);
@@ -29,6 +53,7 @@ const LogsPage: React.FC<logsPageProps> = () => {
   return (
     <>
       <Navbar />
+      <button onClick={DownloadLogs} className="w-[150px] mx-[100px] my-5 bg-blue-500 text-white rounded-full p-2">Download Logs</button>
       <ReadingsTable logs={logs} className="w-[90vw]  mx-auto border-2 border-black border-solid rounded-full" />
     </>
   );
