ef14c9f9f67702fe09df925eed139a362d697d48
sushant-wayal <sushantssr12345@gmail.com>
2024-04-21 03:28:37 +0530

    Add 404 Not Found page with navigation and UI improvements

--- FrontEnd/src/components/Pages/not-found.tsx
+++ FrontEnd/src/components/Pages/not-found.tsx
@@ -1,9 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import Navbar from "../Components/Navbar";
+import { Button } from "../ui/button";
+import { ArrowLeftCircleIcon } from "lucide-react";
+
 interface NotFoundPageProps {}
 
 const NotFoundPage : React.FC<NotFoundPageProps> = () => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <h1>404 Not Found</h1>
+    <div className="h-[88.5vh] w-lvw">
+      <Navbar/>
+      <img className="h-[60vh] w-full" src="../../../not_found.svg" alt="Not Found" />
+      <div className="flex justify-center mt-10 gap-8">
+        <h1 className="text-center text-2xl text-green-600">Requested Information Is not Available</h1>
+        <Button className="bg-green-600" onClick={() => {
+          navigate(-1);
+        }
+        }><ArrowLeftCircleIcon className="mr-2"/>Go Back</Button>
+      </div>
     </div>
   )
 }
